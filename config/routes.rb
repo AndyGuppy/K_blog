e02b2f632406710cc283005c6f5a93080415543e
Kryptonic <kryptonic@Kryptonics-MacBook-Pro.local>
2017-03-22 21:51:53 +0000

    Devise Security added also some basic styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
 
+  
 # Blog Routing
+  devise_for :users
 
-# Redirect to kryptonic_cms at host /admin
-mount KryptonicCms::Engine, at: '/admin'
+# Redirect to kryptonic_cms routes for /admin
+  mount KryptonicCms::Engine, at: '/admin'
 
+# search
   get 'search', to: 'posts#search', as: :search
+
+# Specific blogs by slugs
   get '/:slug', to: 'posts#show', as: :post
 
   root to: 'posts#index'
